d7136e8dfd4ffdf75297d2f6f83db07ef1559193
cokkietw <153282455+cokkietw@users.noreply.github.com>
2024-03-16 19:12:58 +0800

    Add files via upload

--- 2_RNN.py
+++ 2_RNN.py
@@ -1,28 +1,51 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
-import numpy as np
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 
-class NN(nn.Module):  #定义神经网络
-    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
-        super(NN, self).__init__()
+# 定义RNN神经网络
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.flatten = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28*28*3,input_size)
+        )
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
-        self.fc1 = nn.Sequential(  #使定义的池化层形状和全连接层输入的形状一样
-            nn.Flatten(),#压平
-            nn.Linear(2352,input_size),
-            nn.Linear(input_size, hidden_size1),               
-        ) 
-        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-        self.out = nn.Linear(hidden_size2, num_classes)
+    def forward(self, input, hidden):
+        input = self.flatten(input)
+        combined = torch.cat((input, hidden), 1)
+        hidden_output = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden_output
+
+    def initHidden(self,x):
+        return torch.zeros(x, self.hidden_size)
     
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.out(x)
-        return x
+# 定义训练函数
+def train(model, criterion, optimizer, data):
+    model.train()
+    total_loss = 0
+    for i,(images, labels) in enumerate(data):
+        hidden = model.initHidden(images.shape[0])
+        optimizer.zero_grad()
+        loss = 0
+        # print(images.shape,images[0].shape,images[0].view(1, -1),images.size(0))
+        output, hidden = model(images, hidden)
+        loss = criterion(output, labels)
+
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+        if (i+1) % 100 == 0:
+            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
     
-def evaluate_model_metrics(model, test_loader):#指标计算函数
+def evaluate_model_metrics(model, test_loader):#指标计算函数&测试函数
     TP=[0 for i in range (10)]
     TN=[0 for i in range (10)]
     FP=[0 for i in range (10)]
@@ -32,7 +55,8 @@ def evaluate_model_metrics(model, test_loader):#指标计算函数
         for i,(images, labels) in enumerate(test_loader):
             images = images
             labels = labels
-            outputs = model(images)
+            hidden = model.initHidden(images.shape[0])
+            outputs , _ = model(images,hidden)
             predicted = outputs.argmax(1)
             for i in range (len(predicted)):
                 label=labels[i]
@@ -64,44 +88,27 @@ def evaluate_model_metrics(model, test_loader):#指标计算函数
     
     return accuracy, precision, recall, f1_score
 
+# 数据预处理
 transform = transforms.Compose([
     transforms.Resize((28, 28)),
-    transforms.ToTensor(), 
-    transforms.Normalize((0.1307,), (0.3081,))
+    transforms.ToTensor(),
 ])
 
 # 加载数据集
-batch_size = 64
+batch_size=128
 train_dataset = datasets.ImageFolder(root='.\\fashion-mnist\\mnist_train', transform=transform)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_dataset = datasets.ImageFolder(root='.\\fashion-mnist\\mnist_test', transform=transform)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
-model = NN(input_size=28*28, hidden_size1=128, hidden_size2=256, num_classes=10)
-
-# 定义损失函数和优化器
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # 训练模型
-num_epochs=1
-model.train()
+model = RNN(input_size=28*28, hidden_size=128, output_size=10)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 3
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        images = images
-        labels = labels
-        # print(images.shape)
-        # 前向传播
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-
-        # 反向传播和优化
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if (i+1) % 100 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
-print('Training Finished')
+    train(model, criterion, optimizer, train_loader)
 
 # 测试模型
 accuracy, precision, recall, f1_score = evaluate_model_metrics(model, test_loader)
